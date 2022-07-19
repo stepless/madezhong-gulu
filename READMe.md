49b4c6ddc76eac38823b8323e3ad74dcd554484d
luma <madezhong1220@gmail.com>
2022-07-19 17:17:23 +0800

    创建按钮的基本样式

--- READMe.md
+++ READMe.md
@@ -1,1 +1,9 @@
 # 这是一个基于vue的ui框架
+
+## 安装
+在使用本框架前，请在css中开启border-box
+```
+*{
+    box-sizing: border-box;
+}
+```
---
+++ src/button.vue
@@ -0,0 +1,56 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+<script>
+
+</script>
+<style lng="scss" scoped>
+.g-button{
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding:0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background-color: var(--button-bg);
+    vertical-align: middle;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+}
+.g-button:hover{
+    border-color: var(--border-color-hover);
+}
+.g-button:active{
+    background-color: var(--button-active-bg);
+}
+.g-button .icon{
+    order:1;
+    margin-right: .1em;
+    padding-bottom: .05em;
+}
+.g-button .content{
+    order:2;
+    line-height: 1.2em;
+}
+.g-button.icon-right .icon{
+    order:2;
+    margin-right: 0;
+    margin-left: .1em;
+}
+.g-button.icon-right .content{
+    order:1;
+}
+@keyframes spin {
+    0%{
+        transform: rotate(0deg);
+    }
+    100%{
+        transform: rotate(360deg);
+    }
+}
+.loading{
+    animation:spin 2s infinite linear;
+}
+</style>
